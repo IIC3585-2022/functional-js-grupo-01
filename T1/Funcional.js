181d7b0af5a33566aa1c1b12423089c8e3ade2cb
Julián <jgarcg@uc.cl>
2022-03-27 19:01:24 -0300

    ppt y eliminados archivos obsoletos

--- T1/Funcional.js
+++ T1/Funcional.js
@@ -1,36 +1,45 @@
 const readline = require("readline-sync");
 
-const {pipe, compose, K, K2, Y} = require("./Combinators.js");
+const compose = (f,g) => (x) => f(g(x));
 
-// Funcion que inicializa el juego y lo detiene cuando está terminado
-const playGame = (players) => K(jugada(initGame(players)))(console.log(`Juego inicializado por jugadores ${players.slice(0, -1).join(', ') + ' y ' +players.slice(-1)}`));
+const pipe = functions => (data) => functions.reduce( (value, func) => func(value), data);
 
-// Función recursiva que revisa si el juego ha teminado, y en caso contrario llama a newScores y vuelve a revisar si alguien gana recursivamente
-const jugada = Y(f => scores => gameFinished(scores) ? console.log(pipe([winnerPlayers, winDialogue])(scores)) : compose(f, newScores)(scores));
+const composeThree = (f,g,h) => (...x) => f(g(h(...x)));
 
-// Función que inicializa el puntaje de todos los jugadores en 501
-const initGame = (players) => players.map((player) => [player, 501]);
+const S = f => g => (...x) => f(...x)(g(...x));
 
-// Función que revisa si hay algun jugador que haya llegado al puntaje 0.
-const gameFinished = (scores) => scores.find((score) => score[1] === 0) ? true : false;
+const K = x => y => x;
 
-// Funcion que hace log del puntaje actual y después devuelve el puntaje obtenido
-const logScore = name => K2(arg => console.log(`${name} queda con ${Math.abs(arg)} puntos`));
+const imprimir_nuevo_puntaje = (nombre_jugador) => nuevo_puntaje => console.log(`El jugador ${nombre_jugador} ha quedado con ${nuevo_puntaje} puntos`);
 
-// Funcion que genera una jugada (nombre, score) basado en las acciones del turno del jugador
-const insertPlay = (name, score, plays) => [name, logScore(name)(Math.abs(plays.map((play) => 
-    calculate_score(play)).reduce((a, b) => a - b, score)))];
-    
-// Funcion que calcula el puntaje adquirido con una accion del jugador
-const calculate_score = play => play == "DB" ? 50 : (play == "SB" ? 25 : (play == "Null" ? 0 : play[1] * play[0]));
+const ingresar_jugada = (nombre_jugador, puntaje_actual) => 
+S(K)(imprimir_nuevo_puntaje(nombre_jugador))(
+      Math.abs(K(puntaje_actual)(
+        console.log(`Es el turno del jugador ${nombre_jugador} (${puntaje_actual} puntos):`)
+      ) - tirar_veces(3).reduce((a, b) => a + b)
+    ));
 
-// Función que obtiene los nuevos puntajes de cada jugador tomando su puntaje anterior y sus nuevos lanzamientos.
-const newScores = (scores) => scores.map((score) => insertPlay(score[0], score[1], JSON.parse(readline.question(`Ingrese lanzamiento de ${score[0]}:\n `))));
+const tirar_veces = (n) => [...Array(n).keys()].map((value) => get_shot_input(value, NaN))
 
-// Función que devuelve un arreglo con los nombres de los jugadores que tienen puntaje 0.
-const winnerPlayers = (scores) => scores.filter((score) => score[1] === 0).map((winners) => winners[0]);
+const getScore = (n, input) => input == "SB" || input == "DB" ? get_shot_input(n, puntaje_tiro(input)) : 
+                    get_shot_input(n, puntaje_tiro(input, readline.question(`Cual fue el multiplicador?: `).trim()));
 
-// Función que muestra el dialogo final cuando gana un jugador.
+const get_shot_input = (n, result) => isNaN(result) ? getScore(n, readline.question(`Que anotó (${n+1}): `).trim()) : result
+
+const puntaje_tiro = (lanzamiento, multiplicador) => lanzamiento == "DB" ? 50 : (lanzamiento == "SB" ? 25 : Number(lanzamiento.trim()) * Number(multiplicador.trim()));
+
+const gameFinished = (jugadores) => jugadores.some((jugador) => jugador[1] === 0);
+
+const winnerPlayers = (jugadores) => jugadores.filter((jugador) => jugador[1] === 0).map((jugador) => jugador[0])
+
+const newTurn = jugadores => jugadores.map((jugador) => [jugador[0], ingresar_jugada(jugador[0], jugador[1])]);
+
+const continueOrFinish = (jugadores) => gameFinished(jugadores) ? winnerPlayers(jugadores) : play_game(jugadores);
+
+const play_game = (jugadores) => compose(continueOrFinish, newTurn)(jugadores)
+
 const winDialogue = (winners) => winners.length === 1 ? `Ha ganado ${winners[0]}` : `Hay un empate entre ${winners[0] + ' y ' + winners[1]}`;
 
-playGame(['JUAN', 'PETER']);
+const init_game = nombres_jugadores => nombres_jugadores.map(nombre => [nombre, 501]);
+
+console.log(pipe([init_game, play_game, winDialogue])(['Marcos', 'Jose']));
